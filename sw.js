cf8487590bdc27880ccfad028b4075055f80d832
Ten <64129965+liuenqian@users.noreply.github.com>
2026-09-19 17:37:31 +0800

    Refresh cache for volume-only voice mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'labguide-v1.3-20260919';
+const CACHE = 'labguide-v1.3.1-20260919';
 const ASSETS = ['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install', event => {
