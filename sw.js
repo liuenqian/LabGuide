f80b941c022364f2879fa1c3b1cbb966a16da868
Ten <64129965+liuenqian@users.noreply.github.com>
2026-09-19 20:15:20 +0800

    Refresh cache for simplified setup header

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'labguide-v1.4.4-20260919';
+const CACHE = 'labguide-v1.4.5-20260919';
 const ASSETS = ['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install', event => {
