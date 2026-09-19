4639e99e04bb1201c3ac4d227f1d18ddf67c50f8
Ten <64129965+liuenqian@users.noreply.github.com>
2026-09-19 17:35:38 +0800

    Refresh LabGuide v1.3 offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'labguide-v1.2-20260919b';
+const CACHE = 'labguide-v1.3-20260919';
 const ASSETS = ['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install', event => {
